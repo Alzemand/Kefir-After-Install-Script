614e95989a48bb724c0ee1621ae380d9bb20edfc
Edilson Alzemand Sigmaringa Junior <edilson_alz@hotmail.com>
2016-09-11 11:55:36 -0300

    -y

--- kefir-install.sh
+++ kefir-install.sh
@@ -1023,7 +1023,7 @@ case $op in
             sudo apt-get install android-studio -y
             sudo apt-get install cool-retro-term -y
             sudo apt-get install atom -y
-            sudo apt-get install pepperflashplugin-nonfree
+            sudo apt-get install pepperflashplugin-nonfree -y
             sudo apt-get install pipelight-multi -y
             sudo apt-get install flat-plat-gs -y
             sudo apt-get install windos-10-themes -y
